89c7fc1fd85fbccd03c0a4535c79e8ff58c1f10e
Shreya <shreya.goyal@peopletech.com>
2024-08-20 15:57:29 -0700

    Fresh-pull

---
+++ ecz_cdk_repo/self_healing.py
@@ -0,0 +1,42 @@
+from aws_cdk import (
+    Stack,
+    aws_apigateway as apigateway,
+    aws_lambda as _lambda,
+    Duration,
+    aws_iam as iam,
+    CfnOutput,
+    aws_ssm as ssm,
+        aws_iam
+)
+from constructs import Construct
+
+class SelfHealing(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, environment_type: str, account: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+    
+        current_region = self.region
+        default_region = 'us-west-2'
+
+
+
+
+        # Lambda Function
+        lambda_function = _lambda.Function(
+            self, 'lambda_ocr_function',
+            runtime=_lambda.Runtime.PYTHON_3_11,
+            architecture=_lambda.Architecture.ARM_64,
+            code=_lambda.Code.from_asset('backend/lambda/self_healing/'),
+            handler='lambda_function.lambda_handler',
+            function_name=f'self-healing-{environment_type}-{current_region}',
+
+            environment={
+                'environment_type': environment_type
+            },
+            memory_size=3008,
+
+            timeout=Duration.minutes(2)
+        )
+
+       
