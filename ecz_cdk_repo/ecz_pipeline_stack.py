9ff43a77c739f0b66b1a61698c3776d875e20c9d
shreyagoyal-06 <shreya.goyal@peopletech.com>
2024-08-25 15:21:03 -0700

    Update ecz_pipeline_stack.py

--- ecz_cdk_repo/ecz_pipeline_stack.py
+++ ecz_cdk_repo/ecz_pipeline_stack.py
@@ -1,30 +1,24 @@
-from aws_cdk import (
-    Stack,
-    pipelines,
-    aws_iam as iam,
-    aws_logs as logs,
-    Environment
-)
+from aws_cdk import Stack, pipelines, aws_iam as iam, aws_logs as logs, Environment
 from constructs import Construct
 from ecz_cdk_repo.pipeline_stage import self_heal_Stage
 import aws_cdk as cdk
- 
+
 class EczPipelineStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
- 
+
         # GitHub repository information
         owner = "shreyagoyal-06"
-        repo = "CDKApp".
+        repo = "CDKApp".  
         branch = "main"  # or your default branch name
- 
+
         # Create a GitHub source action
         source = pipelines.CodePipelineSource.connection(
             f"{owner}/{repo}",
             branch,
             connection_arn="arn:aws:codeconnections:us-west-2:913524913171:connection/0e6304b1-88f7-4886-8208-1de75317c190"  # Replace with your connection ARN
         )
- 
+
         # Create a CloudWatch log group
         log_group = logs.LogGroup(
             self,
@@ -32,13 +26,13 @@ class EczPipelineStack(Stack):
             log_group_name="/aws/codepipeline/self-heal-app-pipeline",
             retention=logs.RetentionDays.ONE_WEEK
         )
- 
+
         # Create the IAM policy for CloudWatch Logs
         cloudwatch_policy = iam.PolicyStatement(
             actions=["logs:PutLogEvents", "logs:CreateLogStream"],
             resources=[log_group.log_group_arn]
         )
- 
+
         # Create a custom shell script to run commands and log errors
         error_logging_script = """
             set -e
@@ -46,7 +40,7 @@ class EczPipelineStack(Stack):
                 error_message="error: $1"
                 aws logs put-log-events --log-group-name /aws/codepipeline/self-heal-app-pipeline --log-stream-name pipeline-errors --log-events timestamp=$(date +%s%3N),message="$error_message"
             }
- 
+
             npm install -g aws-cdk@2 || { log_error "Failed to install aws-cdk: $?"; exit 1; }
             pip install -r requirements.txt || { log_error "Failed to install requirements: $?"; exit 1; }
             echo "Deploying application using AWS CDK"
@@ -56,7 +50,7 @@ class EczPipelineStack(Stack):
                 exit 1
             }
         """
- 
+
         pipeline = pipelines.CodePipeline(
             self,
             "self-heal-app",
@@ -71,16 +65,16 @@ class EczPipelineStack(Stack):
                 role_policy=[cloudwatch_policy]
             )
         )
- 
+
         envs = {"dev": "913524913171"}
         regions = ["us-west-2"]
- 
+
         for environment_type, account in envs.items():
             wave = pipeline.add_wave(f"{environment_type}-deployment-parallel")
- 
+
             if environment_type != "dev":
                 wave.add_pre(pipelines.ManualApprovalStep(f"promote-to-{environment_type}"))
- 
+
             for region in regions:
                 deploy_apigateway = self_heal_Stage(
                     self,
